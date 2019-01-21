c578150fd72f6e109982f00a75b720372527f838
busan-hyh <yeonhwa001@daum.net>
2019-01-21 23:44:42 +0900

    190121 nodejsbasic

--- 05Taca_node_basic/helloworld.js
+++ 05Taca_node_basic/helloworld.js
@@ -4,9 +4,11 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  //createServer는 서버 생성함수.
+  //서버가 생성되면 아래 함수를 실행하라는 콜백함수▼
+  res.statusCode = 200;//성공을 의미하는 200코드
+  res.setHeader('Content-Type', 'text/plain');//이미지면 image..
+  res.end('Hello World\n');//응답을 이걸로 끝내겠다! 포함문자가 body에 들어감
 });
 
 server.listen(port, hostname, () => {

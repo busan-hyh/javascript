7ecc9d97cf7ee6807774563ce98ed4a4daacb606
busan-hyh <yeonhwa001@daum.net>
2019-02-17 22:59:59 +0900

    19-02-17 js query

--- 07Mysql/connect.js
+++ 07Mysql/connect.js
@@ -10,8 +10,9 @@ var conn        = mysql.createConnection({
 
 conn.connect();
 
-var sql = 'SELECT * FROM TODO_USER';
-conn.query(sql, function(err, rows, fields){
+// SELECT QUERY
+var sqlSelect = 'SELECT * FROM TODO_USER';
+conn.query(sqlSelect, function(err, rows, fields){
     if(err){
         console.log(err);
     } else {
@@ -19,6 +20,39 @@ conn.query(sql, function(err, rows, fields){
             console.log(rows[i].uid);
         }
     }
-})
+});
+
+// INSERT QUERY
+var sqlInsert = 'INSERT INTO TODO_USER (uid, pass, ip) VALUES(? ,?, ?)';
+var paramsI = ['myuid', 'mypass', 'myip'];// ?치환자를 두는 방법. 보안에 활용됨
+conn.query(sqlInsert, paramsI, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else {
+            console.log(rows);
+    }
+});
+
+// UPDATE QUERY
+var sqlUpdate = 'UPDATE TODO_USER SET pass=?, ip=? WHERE uid=?';
+var paramsU = ['yourpass', 'yourip', 'yourid'];
+conn.query(sqlUpdate, paramsU, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(rows);
+    }
+});
+
+// DELETE QUERY
+var sqlDelete = 'DELETE FROM TODO_USER WHERE uid=?';
+var paramsD = ['yourid'];
+conn.query(sqlDelete, paramsD, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(rows);
+    }
+});
 
 conn.end();
